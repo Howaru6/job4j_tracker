fe0b7c73477906c15542783a404ca7c7e2770463
Howaru <Hellwudpecker1707@gmail.com>
2023-01-17 15:43:54 +0300

    1.9. Статические и не статические методы [#504806]

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -3,14 +3,6 @@ package ru.job4j.oop;
 public class Calculator {
     private static int x = 5;
 
-    private static int y;
-
-    private int a;
-
-    private int m;
-
-    private int n;
-
     public static int sum(int y) {
         return x + y;
     }
@@ -20,30 +12,31 @@ public class Calculator {
     }
 
     public static int minus(int m) {
-        return x - m;
+        return m - x;
     }
 
-    public static int divide(int n) {
+    public int divide(int n) {
         return n / x;
     }
 
     public int sumAllOperation(int s) {
-        return y + a + m + n + s;
+        return sum(s) + multiply(s) + minus(s) + divide(s);
     }
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         int rsl = calculator.multiply(5);
-        Calculator calculator1 = new Calculator();
-        int rsl1 = calculator1.sumAllOperation(5);
-        int result = sum(10);
-        int result1 = minus(10);
-        int result2 = divide(10);
-        System.out.println(result);
+        int rsl1 = calculator.sum(5);
+        int rsl2 = calculator.multiply(10);
+        int rsl3 = calculator.minus(15);
+        int rsl4 = calculator.divide(20);
+        int rsl5 = calculator.sumAllOperation(25);
         System.out.println(rsl);
         System.out.println(rsl1);
-        System.out.println(result1);
-        System.out.println(result2);
+        System.out.println(rsl2);
+        System.out.println(rsl3);
+        System.out.println(rsl4);
+        System.out.println(rsl5);
     }
 }
 
