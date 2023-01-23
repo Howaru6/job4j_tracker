c4cedd8ea69cb7c01d74b4fc8696f95c3ccb215d
Howaru <Hellwudpecker1707@gmail.com>
2023-01-23 17:48:44 +0300

    4. Расстояние между точками в трехмерном пространстве. [#122643]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,38 +7,42 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PointTest {
 
     @Test
-    public void when50to20then3() {
-        Point a = new Point(5, 0);
-        Point b = new Point(2, 0);
-        double expected = 3;
-        double out = a.distance(b);
+    public void when540to210to340then3() {
+        Point a = new Point(5, 4, 0);
+        Point b = new Point(2, 1, 0);
+        Point z = new Point(3, 4, 2);
+        double expected = 4.24;
+        double out = a.distance3d(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when73to25then5() {
-        Point a = new Point(7, 3);
-        Point b = new Point(2, 5);
-        double expected = 5.38;
-        double out = a.distance(b);
+    public void when111to200to002then5() {
+        Point a = new Point(1, 1, 1);
+        Point b = new Point(2, 0, 0);
+        Point z = new Point(0, 0, 2);
+        double expected = 1.73;
+        double out = a.distance3d(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when00to20then2() {
-        Point a = new Point(0, 0);
-        Point b = new Point(2, 0);
-        double expected = 2;
-        double out = a.distance(b);
+    public void when963to842to753then2() {
+        Point a = new Point(9, 6, 3);
+        Point b = new Point(8, 4, 2);
+        Point z = new Point(7, 5, 3);
+        double expected = 2.44;
+        double out = a.distance3d(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
-    public void when77to22then7() {
-        Point a = new Point(7, 7);
-        Point b = new Point(2, 2);
-        double expected = 7.07;
-        double out = a.distance(b);
+    public void when000to001tp002then7() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 1);
+        Point z = new Point(0, 0, 2);
+        double expected = 1;
+        double out = a.distance3d(b);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
