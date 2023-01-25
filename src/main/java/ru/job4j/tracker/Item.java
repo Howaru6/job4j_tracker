b03e9af17cfbf4e9e18266be5648c8e84d678d6c
Howaru <Hellwudpecker1707@gmail.com>
2023-01-25 16:21:50 +0300

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,9 @@ public class Item {
     private int id;
     private String name;
 
+    public Item() {
+    }
+
     public int getId() {
         return id;
     }
@@ -19,4 +22,23 @@ public class Item {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+    public void printinfo() {
+        System.out.println("Имя ползователя: " + name);
+        System.out.println("ID ползователя: " + id);
+    }
+
+    public static void main(String[] args) {
+        Item first = new Item("Олег",12802341);
+        first.printinfo();
+    }
+
 }
