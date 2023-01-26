5e81c9341139dfb0268c9264cc9902f2ec53b1df
Howaru <Hellwudpecker1707@gmail.com>
2023-01-26 23:26:55 +0300

    8. Date. Отображение даты. [#310017 #385327]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -20,11 +20,8 @@ public class Item {
 
     private String name;
 
-    LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
 
     public int getId() {
         return id;
@@ -40,6 +37,10 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public void printinfo() {
