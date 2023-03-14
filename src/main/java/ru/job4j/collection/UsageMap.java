0259906f941422107ce71f1b21176c0755b48280
Howaru <Hellwudpecker1707@gmail.com>
2023-03-14 17:31:06 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("Kadochnikov@yandex.ru", "Igor Kadochnikov");
+        map.put("Kadochnikov@yandex.ru", "Igor Kadochnikov");
+        map.put("Kadochnikov@yandex.ru", "Igor Kadochnikov");
         for (String key : map.keySet()) {
             String value = map.get(key);
             System.out.println(key + " = " + value);
