fe1434987c20f5a8e24a635d8c287ad4cc68d7b1
Howaru <Hellwudpecker1707@gmail.com>
2023-03-20 19:40:07 +0300

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -7,18 +7,37 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Данный класс описывает сервис банка, с помощью которого
+ * пользователь может управлять своими банковскими счетами.
+ */
 public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Добавляет нового пользователя в список пользователей.
+     * @param user добавляется в список.
+    */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Удаляет пользователя из карты пользователей
+     * по номеру паспорта.
+     * @param passport номер паспорта.
+     * @return статус нахождения и удаления пользователя.
+     */
     public boolean deleteUser(String passport) {
         return users.remove(new User(passport, "")) != null;
 
     }
 
+    /**
+     * Добавляет новый банковский счет для пользователя по номеру паспорта.
+     * @param passport номер паспорта.
+     * @param account профиль пользователя.
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -29,6 +48,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Находит паспорт по его номеру.
+     * @param passport номер паспорта.
+     * @return найденный по номеру паспор или null, если паспорт не найден.
+     */
     public User findByPassport(String passport) {
         User findUser = null;
         for (User user : users.keySet()) {
@@ -40,6 +64,12 @@ public class BankService {
         return findUser;
     }
 
+    /**
+     * Находит банковский счет по номеру паспорта и счету.
+     * @param passport номер паспорта.
+     * @param requisite номер счета.
+     * @return профиль пользователя или null, если профиль не был найден.
+     */
     public Account findByRequisite(String passport, String requisite) {
         Account findAccount = null;
         User user = findByPassport(passport);
@@ -54,6 +84,15 @@ public class BankService {
         return findAccount;
     }
 
+    /**
+     * Переводит деньги с одного счета на другой.
+     * @param srcPassport номер паспорта отправителя.
+     * @param srcRequisite номер счета отправителя.
+     * @param destPassport номер паспорта получателя.
+     * @param destRequisite номер счета получателя.
+     * @param amount сумма перевода.
+     * @return статус перевода.
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
@@ -67,6 +106,11 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Банковский счета которые есть у пользователя.
+     * @param user пользователь.
+     * @return список счетов пользователя, если они есть.
+     */
     public List<Account> getAccounts(User user) {
         return users.get(user);
     }
